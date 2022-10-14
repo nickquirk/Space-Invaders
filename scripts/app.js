09052a97533dc76bea547ee4f10402a42628facd
Nick Quirk <n.quirk87@gmail.com>
2022-10-14 14:32:57 +0100

    added base project files

---
+++ scripts/app.js
@@ -0,0 +1,82 @@
+function init() {
+// ! ELEMENTS
+// start button
+// reset
+// grid
+// grid cells
+// lives display span
+// score display span
+
+
+//functions 
+//datatypes - arrays? numbers? 
+//outline of function to move enemies 
+
+// ! VARIABLES 
+// score
+// lives
+
+// ? Grid variables
+// width of grid
+// height of grid
+
+// ? Character variables
+// Starting position 
+// Current position 
+
+// ? Enemy variables 
+// Starting position 
+// Current position 
+// Next position 
+// Position of neighbour/s
+
+// ? Projectile variables
+// Player projectile current position 
+// Enemy projectile current position 
+// Has collided? 
+
+
+// ! FUNCTIONS
+// ? Create game grid 
+// Create element dynamically using createElement
+// Display index of grid element
+// Add the index as a data-id
+// Append the new cell to the grid 
+
+// ? Start game function 
+// Start game timer : 2000ms 
+//        - Spawn enemy 
+//        - Spawn player character 
+//        - Start aliens moving 
+//        - Reset lives + points
+
+// ? Reset game function 
+
+
+// ? Player functions 
+// Spawn - inital positon 
+// Movement
+// Fire projectile 
+// is Hit?
+
+
+// ? Enemy functions 
+// Spawn - initial position 
+// Movement
+// Fire projectile 
+// Is hit? 
+
+// ? End game screen 
+
+// ! EVENTS
+// Start button
+// Reset button
+
+// Keyboard input 
+//      - Arrow keys to move
+//      - Space to fire
+  
+
+}
+
+window.addEventListener('DOMContentLoaded', init)
